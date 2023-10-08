7b211499a7a28b1bbeaad267c6d87de90a40b1e5
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-08 11:55:54 -0300

    módulos aula04
    separado as funtions em modulos exportado usando metodo module.exports carregado no arquivo calc usando require

---
+++ modulos/calc.js
@@ -0,0 +1,9 @@
+var somarFunc = require("./somar");
+var divFunc = require("./div");
+var multiFunc = require("./multi");
+var subFunc = require("./sub");
+
+console.log(somarFunc(1, 2));
+console.log(divFunc(1, 2));
+console.log(multiFunc(1, 2));
+console.log(subFunc(1, 2));
