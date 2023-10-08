2c92b44dd76b02c58978e5fdaef7b230a99a3850
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-08 13:22:36 -0300

    http aula05
    criado servidor local

--- htpp/http.js
+++ htpp/http.js
@@ -1,7 +1,9 @@
 var htpp = require("http");
 
-htpp.createServer(function (req, res) {
-    res.end("Ola")
-}).listen(8081);
+htpp
+  .createServer(function (req, res) {
+    res.end("Ola");
+  })
+  .listen(8081);
 
 console.log("O Servidor esta rodado");
