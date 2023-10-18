9c6aa68f6be330bf28e31802dad91025a93da8ad
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-18 14:43:28 -0300

    criador formulario

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+// Configuration
+// Template Engine
+const handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Connection to the MySQL database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("test", "root", "1234", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+// Routes
+app.get("/card", function (req, res) {
+  res.render("formulario");
+});
+
+app.listen(8081, function () {
+  console.log("Servidor rodando na URL http://localhost:8081");
+});
